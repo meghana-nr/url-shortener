078a02c304124efb30f5f70cfaba2fd7fcafaba5
Meghana NR <nmeghana@nmeghana-a01.vmware.com>
2022-08-01 23:00:57 +0530

    Final commit

---
+++ test/test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const expect = require('chai').expect
+const request = require('supertest');
+const app = require('../server')
+
+describe('Unit testing the / route', function() {
+
+    it('should return OK status', function() {
+      return request(app)
+        .get('/')
+        .then(function(response){
+            assert.equal(response.status, 200)
+        })
+    });
+
+});
